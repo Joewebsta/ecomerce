beab08b6e8f004cd64d748b91fb603599b030be6
Joe Webster <joseph.scott.webster@gmail.com>
2023-05-13 14:48:43 -0400

    Create products GET api endpoint

--- app.js
+++ app.js
@@ -1,12 +1,27 @@
 const express = require("express");
 const app = express();
+const { Client } = require('pg');
+const morgan = require('morgan');
+
+app.use(express.json());
+app.use(morgan('dev'));
+
 
 app.get("/ping", (req, res) => {
   res.status(200).send("pong");
 });
 
-app.get("/api/products", (req, res) => {
-  res.status(200).send("products");
+app.get("/api/products", async (req, res) => {
+  try {
+    const client = new Client({ database: 'ecommerce'});
+    client.connect();
+    const result = await client.query('SELECT * FROM products');
+    res.json(result.rows);
+    client.end();
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'An error occurred while retrieving products' });
+  }
 });
 
 module.exports = app;
