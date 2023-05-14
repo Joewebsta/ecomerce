c9e1a30147a532c8405010478d37fd87140d3232
Joe Webster <joseph.scott.webster@gmail.com>
2023-05-14 14:40:30 -0400

    Display products

--- backend/app.js
+++ backend/app.js
@@ -2,8 +2,10 @@ const express = require("express");
 const app = express();
 const { Client } = require('pg');
 const morgan = require('morgan');
+const cors = require('cors');
 
 app.use(express.json());
+app.use(cors());
 app.use(morgan('dev'));
 
 app.get("/ping", (req, res) => {
@@ -11,8 +13,8 @@ app.get("/ping", (req, res) => {
 });
 
 app.get("/api/products", async (req, res) => {
-  const client = new Client({ database: 'ecommerce'});
-  
+  const client = new Client({ database: 'ecommerce' });
+
   try {
     await client.connect();
 
@@ -22,20 +24,20 @@ app.get("/api/products", async (req, res) => {
     console.error(error);
     res.status(500).json({ error: 'An error occurred while retrieving products' });
   } finally {
-    client.end();  
+    client.end();
   }
 });
 
 app.post("/api/products", async (req, res) => {
-  const client = new Client({ database: 'ecommerce'});
-  
+  const client = new Client({ database: 'ecommerce' });
+
   try {
     await client.connect();
 
-    const {name, price, category} = req.body;
+    const { name, price, category } = req.body;
     const query = "INSERT INTO products(name, price, category) VALUES($1, $2, $3)";
     const values = [name, price, category];
-    
+
     await client.query(query, values);
 
     res.status(201).json({ message: 'Product created successfully' });
@@ -43,12 +45,12 @@ app.post("/api/products", async (req, res) => {
     console.error(error);
     res.status(500).json({ error: 'An error occurred while creating the product' });
   } finally {
-    client.end(); 
+    client.end();
   }
 });
 
 app.delete("/api/products/:id", async (req, res) => {
-  const client = new Client({ database: 'ecommerce'});
+  const client = new Client({ database: 'ecommerce' });
 
   try {
     await client.connect();
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,42 @@
-import { useState } from 'react'
-import React from 'react'
+import React, { useState } from 'react'
+import { useEffect } from 'react'
+import axios from 'axios'
 
-function App() {
+const ProductList = ({ products }) => {
+  const productList = products.map(product => {
+    return <Product key={product.id} product={product} />
+  });
+
+  return <ul>{productList}</ul>;
+}
+
+const Product = ({ product }) => {
+  const { id, name, price, category } = product;
+  return <li key={id}>
+    <p>{name}{price}{category}</p>
+    <button>delete</button>
+  </li>
+}
+
+const App = () => {
+  const [products, useProducts] = useState([]);
+
+  useEffect(() => {
+    const getProducts = async () => {
+      const response = await axios.get('http://localhost:3000/api/products');
+      console.log(response.data);
+      useProducts(response.data);
+
+    }
+
+    getProducts();
+  }, []);
+
+
   return (
     <>
       <h1>Hello world!</h1>
+      <ProductList products={products} />
     </>
   )
 }
